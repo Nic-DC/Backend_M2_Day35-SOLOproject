726c03c3914f0ce7bffb1c1697528a86d1881546
Nic <nic.dan.costea@gmail.com>
2023-02-17 12:40:00 +0100

    password difficulty & register message

--- client/src/pages/authentication/forms/Register.jsx
+++ client/src/pages/authentication/forms/Register.jsx
@@ -3,6 +3,7 @@ import { Form, Button } from "react-bootstrap";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import zxcvbn from "zxcvbn";
 
 //import passport from "passport";
 
@@ -13,15 +14,30 @@ function Register() {
   });
 
   const [isRegistered, setIsRegistered] = useState(false);
+  const [passwordStrength, setPasswordStrength] = useState(0);
+
+  const passwordStrengthArray = ["Weak", "Fair", "Good", "Strong", "Very strong"];
+
+  const calculatePasswordStrength = (password) => {
+    const result = zxcvbn(password);
+    setPasswordStrength(result.score);
+  };
 
   console.log("formData", formData);
 
   const handleChange = (event) => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
+    if (event.target.name === "password") {
+      calculatePasswordStrength(event.target.value);
+    }
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (passwordStrength < 3) {
+      toast.error("Password is too weak. Please choose a stronger password.");
+      return;
+    }
     const options = {
       method: "POST",
       headers: {
@@ -38,6 +54,8 @@ function Register() {
       const data = await response.json();
       console.log(data);
       setIsRegistered(true);
+
+      toast.success("Registration successful!");
     } catch (error) {
       console.error("There was a problem with the fetch operation:", error);
       const errorMessage = "Registration failed. Please try again later.";
@@ -65,6 +83,9 @@ function Register() {
           value={formData.password}
           onChange={handleChange}
         />
+        <Form.Text className={`text-${passwordStrength >= 3 ? "success" : "danger"}`}>
+          Password strength: {passwordStrengthArray[passwordStrength]}
+        </Form.Text>
       </Form.Group>
       <Button variant="outline-dark" type="submit">
         Register
