55a560d53f4a567451ab34a562318676ffedbcf5
Nic <nic.dan.costea@gmail.com>
2023-02-17 15:17:23 +0100

    verify valid JWT

--- server/_tests_/auth.test.js
+++ server/_tests_/auth.test.js
@@ -2,6 +2,8 @@ import server from "../src/server";
 import supertest from "supertest";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import TravelUsersModel from "../src/api/USER/model.js";
 
 dotenv.config();
@@ -17,7 +19,13 @@ beforeAll(async () => {
 
   console.log("MONGO_URL_TEST: ", process.env.MONGO_URL_TEST);
 
-  const user = new TravelUsersModel({ email: "jest@test.com", password: "1$}{blalblabla", role: "Guest" });
+  const hashedPassword = await bcrypt.hash("1$}{blalblabla", 12);
+
+  const user = new TravelUsersModel({
+    email: "jest@test.com",
+    password: hashedPassword,
+    role: "Guest",
+  });
   await user.save();
 });
 
@@ -27,8 +35,30 @@ afterAll(async () => {
 });
 
 describe(`M5-Day95-SOLO:`, () => {
+  // Testing the DB connection
   it("should return 200 OK", async () => {
     const response = await client.get("/users");
     expect(response.status).toBe(200);
+  });
+
+  // Test #2
+  it("should return 201 and a valid JWT token with a valid request", async () => {
+    const response = await client
+      .post("/auth/register")
+      .send({
+        email: "jest2@test.com",
+        password: "1$}{blalblabla",
+        role: "Guest",
+      })
+      .expect(201);
+    console.log("response.body", response.body);
+
+    expect(response.body.accessToken).toBeDefined();
+
+    const payload = jwt.verify(response.body.accessToken, process.env.JWT_SECRET);
+    console.log("payload", payload);
+
+    expect(response.body.newUser.email).toEqual("jest2@test.com");
+    expect(response.body.newUser.role).toEqual("Guest");
   });
 });
